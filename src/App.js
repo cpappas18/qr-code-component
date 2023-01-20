eaac8aab6bd93f1ec133d5df29e0c8f5f123a103
Chloe Pappas <chloeoliviapappas@gmail.com>
2023-01-20 12:58:21 -0500

    Moved static image asset to src

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
     return (
         <div className="App">
             <div className="tile">
-                <img src="image-qr-code.png" alt="QR code" className="qr-code"/>
+                <img src={require("./image-qr-code.png")} alt="QR code" className="qr-code"/>
                 <div className="text-container">
                     <h1>Improve your front-end skills by building projects</h1>
                     <h2>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</h2>
