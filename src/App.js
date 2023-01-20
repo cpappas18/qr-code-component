3a2713622e6cc3699881fa14ac1f82f39fad4ae4
Chloe Pappas <chloeoliviapappas@gmail.com>
2023-01-20 11:29:48 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="tile">
+                <img src="image-qr-code.png" alt="QR code" className="qr-code"/>
+                <div className="text-container">
+                    <h1>Improve your front-end skills by building projects</h1>
+                    <h2>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</h2>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
